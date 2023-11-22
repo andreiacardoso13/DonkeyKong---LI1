565118120e6840b0aa58bbb515d3e766e0cd70c4
a107382 <a107382@alunos.uminho.pt>
2023-11-22 16:13:47 +0000

    Tarefa3: importadas librarias; Main: adicionadas imagens para mapa e alguns colecionáveis -- alteração da função main (funções não definidas)

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -9,6 +9,8 @@ Módulo para a realização da Tarefa 3 de LI1 em 2023/24.
 module Tarefa3 where
 
 import LI12324
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
 
 movimenta :: Semente -> Tempo -> Jogo -> Jogo
 movimenta = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,31 @@
 module Main where
 
-main :: IO ()
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import LI12324
+
+{- main :: IO ()
 main = do
   putStrLn "Hello, PrimateKong!"
+-}
+
+get_images :: IO [Picture]
+get_images = do
+               escada              <- loadBMP "imagens/ladder.bmp"
+               alcapao             <- loadBMP "imagens/alcapao.bmp"
+               plataforma          <- loadBMP "imagens/plataforma.bmp"
+               estrela             <- loadBMP "imagens/star.bmp"
+               moeda               <- loadBMP "imagens/coin.bmp"
+               let images = [escada, alcapao, plataforma, estrela, moeda]
+               return images
+
+main :: IO ()
+main = do 
+        imagens <- get_images
+        play  janela                          -- janela onde irá decorrer o jogo
+              bg                              -- cor do fundo da janela
+              fr                              -- frame rate
+              estadoinicial                   -- define estado inicial do jogo
+              desenhaEstado                   -- desenha o estado do jogo
+              reageEvento                     -- reage a um evento
+              reageTempo                      -- reage ao passar do tempo
