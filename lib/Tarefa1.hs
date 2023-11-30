543a7fe207eeaae0dd8d64da848f5ce97585d206
a107382 <a107382@alunos.uminho.pt>
2023-11-30 12:16:28 +0000

    definição da função colisoesPersonagens

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -11,12 +11,14 @@ module Tarefa1 where
 import LI12324
 
 hitbox :: Personagem -> Hitbox
-hitbox (Personagem {posicao = (x,y), tamanho = (c,l)}) = (((x+(c/2)),(y+(c/2))),((x-(c/2)), y-((c/2))))
-
-
+hitbox (Personagem {posicao = (x,y), tamanho = (c,l)}) = ((x-(c/2),y-(l/2)),(x+(c/2), y+(l/2)))
 
 colisoesParede :: Mapa -> Personagem -> Bool
-colisoesParede = undefined
+colisoesParede (Mapa (_,_) _ _) p = undefined 
+-- true if the 1st htbx coord is on the left border or the 2nd htbx coord is on the right border 
 
 colisoesPersonagens :: Personagem -> Personagem -> Bool
-colisoesPersonagens = undefined
+colisoesPersonagens p1@(Personagem {posicao = (x1,y1), tamanho = (c1,l1)}) p2@(Personagem {posicao = (x2,y2), tamanho = (c2,l2)}) = compHtbx hb1 hb2 || compHtbx hb2 hb1
+    where hb1 = hitbox p1
+          hb2 = hitbox p2
+          compHtbx hb1 hb2 = fst(fst hb1) >= fst(fst hb2) && fst(fst hb1) <= fst(snd hb2) && snd(fst hb1) >= snd(fst hb2) && snd(fst hb1) <= snd(snd hb2)
