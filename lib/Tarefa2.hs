d97858a7edf737f2ac31524004319bdf60b4fb26
andreia <a106915@alunos.uminho.pt>
2023-12-03 17:09:37 +0000

    Adição de valida1

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -12,3 +12,46 @@ import LI12324
 
 valida :: Jogo -> Bool
 valida = undefined
+
+valida1 :: Jogo -> Bool
+valida1 (Jogo {mapa = Mapa _ _ (h:t)}) = valida1Aux (last t)
+
+valida1Aux :: [Bloco] -> Bool
+valida1Aux [] = True
+valida1Aux (h:t) | h == Plataforma = valida1Aux t
+                 | otherwise = False
+
+
+{-
+data Mapa =
+  Mapa (Posicao, Direcao) Posicao [[Bloco]]
+  deriving (Eq, Read, Show)
+
+
+data Jogo =
+  Jogo
+    { mapa          :: Mapa -- ^ mapa do jogo
+    , inimigos      :: [Personagem] -- ^ lista de inimigos no mapa
+    , colecionaveis :: [(Colecionavel, Posicao)] -- ^ lista de colecionaveis espalhados pelo mapa
+    , jogador       :: Personagem -- ^ o jogador
+    }
+  deriving (Eq, Read, Show)
+  
+  hitbox :: Personagem -> Hitbox
+hitbox (Personagem {posicao = (x,y), tamanho 
+
+data Personagem =
+  Personagem
+    { velocidade :: Velocidade
+    , tipo       :: Entidade
+    , posicao    :: Posicao
+    , direcao    :: Direcao
+    , tamanho    :: (Double, Double)
+    , emEscada   :: Bool -- ^ se está numa escada
+    , ressalta   :: Bool
+    , vida       :: Int -- ^ não negativo
+    , pontos     :: Int
+    , aplicaDano :: (Bool, Double) -- ^ se está armado e por quanto tempo ainda
+    }
+  deriving (Eq, Read, Show)
+  -}
