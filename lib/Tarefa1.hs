9b0164d58b72b5fde71801fb5cc888b8f22cde63
a107382 <a107382@alunos.uminho.pt>
2023-12-06 10:44:58 +0000

    definição da função colisoesParede, da sua função auxiliar mapaChao (não terminada), e da função auxiliar procuraBlocoInf

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -23,14 +23,21 @@ mapaLimites (Mapa _ _ blocos) p = eLim <= 0 || dLim >= fromIntegral mapaLargura 
   -- "fromIntegral" faz os valores mapaLargura e mapaAltura serem doubles como os valores das coords
 
 mapaChao :: Mapa -> Personagem -> Bool
-mapaChao (Mapa _ _ blocos) p = undefined
+mapaChao (Mapa _ _ blocos) p@(Personagem {posicao = (x,y)}) = blc == Plataforma && snd(fst(hitbox p)) == snd(snd(hitbox blc))
+      where blc = procuraBlocoInf blocos (x,y)
+-- hitbox is only defined for characters, not blocks!! 
+
+procuraBlocoInf :: [[Bloco]] -> Posicao -> Bloco
+procuraBlocoInf ((h:t):ls) (x,y) | y > 0 = procuraBlocoInf ls (x,y-1)
+                              | x > 0 = procuraBlocoInf [t] (x-1,y)
+                              | otherwise = h
 
 colisoesParede :: Mapa -> Personagem -> Bool
-colisoesParede (Mapa {}) p = undefined 
+colisoesParede m@(Mapa _ _ blocos) p@(Personagem {posicao = (x,y)}) = mapaLimites m p || mapaChao m p
 -- true if the 1st htbx coord is on the left border or the 2nd htbx coord is on the right border 
 
 colisoesPersonagens :: Personagem -> Personagem -> Bool
 colisoesPersonagens p1 p2 = compHtbx hb1 hb2 || compHtbx hb2 hb1
     where hb1 = hitbox p1
           hb2 = hitbox p2
-          compHtbx hb1 hb2 = fst(fst hb1) >= fst(fst hb2) && fst(fst hb1) <= fst(snd hb2) && snd(fst hb1) >= snd(fst hb2) && snd(fst hb1) <= snd(snd hb2)
+          compHtbx hb1 hb2 = fst (fst hb1) >= fst (fst hb2) && fst (fst hb1) <= fst (snd hb2) && snd (fst hb1) >= snd (fst hb2) && snd (fst hb1) <= snd (snd hb2)
