26fcd5511a7b16549b3d6db5c464b3577dcd4fa0
andreia <a106915@alunos.uminho.pt>
2023-12-06 16:42:25 +0000

    atualizações no mapa

--- lib/mapafinal.hs
+++ lib/mapafinal.hs
@@ -3,23 +3,15 @@ import LI12324
 import Main
 import Graphics.Gloss
 
-p::Bloco
-p = Plataforma
-
-v::Bloco
-v=Vazio
-
-e::Bloco
-e=Escada
 
 mapaPrincipal :: Mapa 
 mapaPrincipal = Mapa ((0.5,5.5), Oeste) (0.5,2.5)
-                 [[p, p, p, p, p, p, p, p, p, p]
-                 ,[v, v, v, v, v, v, v, v, v, v]
-                 ,[v, v, v, v, v, v, v, v, v, v]
-                 ,[p, p, v, v, v, v, p, p, p, p]
-                 ,[v, v, v, v, v, v, v, v, e, v]
-                 ,[v, v, v, v, v, v, v, v, e, v]
-                 ,[p, p, p, p, p, p, p, p, p, p]
+                 [[Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma]
+                 ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio]
+                 ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio]
+                 ,[Plataforma, Plataforma, Vazio, Vazio, Vazio, Vazio, Plataforma, Plataforma, Plataforma, Plataforma]
+                 ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Escada, Vazio]
+                 ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Escada, Vazio]
+                 ,[Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma]
                  ]
 
