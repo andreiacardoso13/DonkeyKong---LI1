3dbcf5fddeb366136aa5488957f5ff9d08c9c397
andreia <a106915@alunos.uminho.pt>
2023-12-08 22:25:48 +0000

    adicao de ataqueJogador

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -29,104 +29,84 @@ sendo esta no meu tamanho do menor retângulo que contém um personagem
 hitboxDano :: Personagem -> Hitbox
 hitboxDano (Personagem {posicao = (x,y), tamanho = (l,a), direcao = dir}) | dir == Oeste = ((x-(3*l/2),y-(a/2)),(x - (l/2), y + (a/2)))
                                                                           | dir == Este = ((x + (l/2),y-(a/2)),(x + (3*l/2),y + (a/2)))
-{-
-movimenta1' :: Jogo -> Jogo
-movimenta1' (Jogo {inimigos = ((Personagem {posicao = (xInimigo,yInimigo)
-                                          ,tamanho = (lInimigo,aInimigo)
-                                          ,vida = vid}) :t )
-                  ,jogador = (Personagem {posicao = (xJogador, yJogador)
-                                         ,direcao = dir
-                                         ,tamanho = (lPersonagem, aPersonagem)
-                                         ,aplicaDano = (aplica, tempo)
-                                         }
-                             )
-                   }
-             ) 
-    | aplica == True = if colisaoHitbox (hitbox (Personagem {posicao = (xInimigo,yInimigo)
-                                                            ,tamanho = (lInimigo,aInimigo)
-                                                            ,vida = vid})) 
-                                        (hitboxDano (Personagem {posicao = (xJogador, yJogador)
-                                                                ,direcao = dir
-                                                                ,tamanho = (lPersonagem, aPersonagem)
-                                                                ,aplicaDano = (aplica, tempo)}))
-                          then (Jogo {inimigos = (Personagem {posicao = (xInimigo,yInimigo)
-                                                             ,tamanho = (lInimigo,aInimigo)
-                                                             ,vida = vid-1})
-                                     ,jogador = (Personagem {posicao = (xJogador, yJogador)
-                                                            ,direcao = dir
-                                                            ,tamanho = (lPersonagem, aPersonagem)
-                                                            ,aplicaDano = (aplica, tempo)})}) 
-                         else 
-    | otherwise = (Jogo {inimigos = ((Personagem {posicao = (xInimigo,yInimigo)
-                                          ,tamanho = (lInimigo,aInimigo)
-                                          ,vida = vid}) :t )
-                        ,jogador = (Personagem {posicao = (xJogador, yJogador)
-                                               ,direcao = dir
-                                               ,tamanho = (lPersonagem, aPersonagem)
-                                               ,aplicaDano = (aplica, tempo)
-                                               }
-                                   )
-                        }
-                   ) 
--}
-função :: [Personagem] -> Personagem -> [Personagem]
-função [] _ = []
-função ((Personagem {posicao = (xInimigo,yInimigo)
-                    ,tamanho = (lInimigo,aInimigo)
-                    ,vida = vid}) :t) 
-        (Personagem {posicao = (xJogador, yJogador)
-                    ,direcao = dir
-                    ,tamanho = (lPersonagem, aPersonagem)
-                    ,aplicaDano = (aplica, tempo)
-                    }
-       )
-    | colisaoHitbox (hitbox (Personagem {posicao = (xInimigo,yInimigo)
+
+
+ataqueJogador :: [Personagem] -> Personagem -> [Personagem]
+ataqueJogador [] _ = []
+ataqueJogador ((Personagem {velocidade = vI
+                           ,tipo = entI
+                           ,posicao = (xInimigo,yInimigo)
+                           ,direcao = dirI
+                           ,tamanho = (lInimigo,aInimigo)
+                           ,emEscada = escI
+                           ,ressalta = resI
+                           ,vida = vidI
+                           ,pontos = pI
+                           ,aplicaDano = aplicaI
+                           }
+               ) :t) 
+              (Personagem {posicao = (xJogador, yJogador)
+                          ,direcao = dirJ
+                          ,tamanho = (lJogador, aJogador)
+                          ,aplicaDano = (aplicaJ, tempo)
+                          }
+              )
+    | colisaoHitbox (hitbox (Personagem {velocidade = vI
+                                        ,tipo = entI
+                                        ,posicao = (xInimigo,yInimigo)
+                                        ,direcao = dirI
                                         ,tamanho = (lInimigo,aInimigo)
-                                        ,vida = vid
-                                        }
+                                        ,emEscada = escI
+                                        ,ressalta = resI
+                                        ,vida = vidI
+                                        ,pontos = pI
+                                        ,aplicaDano = aplicaI
+                                         }
                             )
                     )
                     (hitboxDano (Personagem {posicao = (xJogador, yJogador)
-                                            ,direcao = dir
-                                            ,tamanho = (lPersonagem, aPersonagem)
-                                            ,aplicaDano = (aplica, tempo)
+                                            ,direcao = dirJ
+                                            ,tamanho = (lJogador, aJogador)
+                                            ,aplicaDano = (aplicaJ, tempo)
                                             }
                                 )
                     )
-                    = (Personagem {posicao = (xInimigo,yInimigo)
+                    = (Personagem {velocidade = vI
+                                  ,tipo = entI
+                                  ,posicao = (xInimigo,yInimigo)
+                                  ,direcao = dirI
                                   ,tamanho = (lInimigo,aInimigo)
-                                  ,vida = vid-1}) : função t (Personagem {posicao = (xJogador, yJogador)
-                                                                       ,direcao = dir
-                                                                       ,tamanho = (lPersonagem, aPersonagem)
-                                                                       ,aplicaDano = (aplica, tempo)
-                                                                       }
-                                                            )
-    | otherwise = (Personagem {posicao = (xInimigo,yInimigo)
+                                  ,emEscada = escI
+                                  ,ressalta = resI
+                                  ,vida = vidI - 1
+                                  ,pontos = pI
+                                  ,aplicaDano = aplicaI
+                                   }
+                      ) : ataqueJogador t (Personagem {posicao = (xJogador, yJogador)
+                                                      ,direcao = dirJ
+                                                      ,tamanho = (lJogador, aJogador)
+                                                      ,aplicaDano = (aplicaJ, tempo)
+                                                      }
+                                          )
+    | otherwise = (Personagem {velocidade = vI
+                              ,tipo = entI
+                              ,posicao = (xInimigo,yInimigo)
+                              ,direcao = dirI
                               ,tamanho = (lInimigo,aInimigo)
-                              ,vida = vid-1}) : função t (Personagem {posicao = (xJogador, yJogador)
-                                                                     ,direcao = dir
-                                                                     ,tamanho = (lPersonagem, aPersonagem)
-                                                                     ,aplicaDano = (aplica, tempo)
-                                                                     }
-                                                         )
+                              ,emEscada = escI
+                              ,ressalta = resI
+                              ,vida = vidI
+                              ,pontos = pI
+                              ,aplicaDano = aplicaI
+                              }
+                   ) : ataqueJogador t (Personagem {posicao = (xJogador, yJogador)
+                                                   ,direcao = dirJ
+                                                   ,tamanho = (lJogador, aJogador)
+                                                   ,aplicaDano = (aplicaJ, tempo)
+                                                   }
+                                       )
         
-{-
-movimentaaa :: Jogo -> Jogo
-movimentaaa (Jogo {inimigos = ((Personagem {vida = vidaInimigo}) : t)
-                  ,jogador = (Personagem {aplicaDano = (aplica, tempo)})
-                  }
-            ) 
-    | aplica == True && tempo /= 0 = if colisaoHitbox (hitbox (Personagem {vida = vidaInimigo})) (hitboxDano (Personagem {aplicaDano = (aplica, tempo)}))
-                                       then movimentaaa (Jogo {inimigos = ((Personagem {vida = vidaInimigo-1}) : t)
-                                                              ,jogador = (Personagem {aplicaDano = (aplica, tempo)})
-                                                              })
-                                       else movimentaaa (Jogo {inimigos = ((Personagem {vida = vidaInimigo}) : t)
-                  ,jogador = (Personagem {aplicaDano = (aplica, tempo)})
-                  })
-    | otherwise = movimentaaa (Jogo {inimigos = ((Personagem {vida = vidaInimigo}) : t)
-                  ,jogador = (Personagem {aplicaDano = (aplica, tempo)})
-                  })
--}
+
 {-|
 
 Varifica se duas hitbox estão em colisão
@@ -138,10 +118,10 @@ Varifica se duas hitbox estão em colisão
 >>>colisaoHitbox ((1,4),(3,1)) ((4,2),(5,0)) =False
 -}
 colisaoHitbox :: Hitbox -> Hitbox -> Bool
-colisaoHitbox ((x1,y1),(x2,y2)) ((x3,y3),(x4,y4)) | x1 >= x3 && x1 <= x4 && y1 <= y3 && y1 >= y4 = True -- ponto inferior esquerdo
-                                                  | x2 >= x3 && x2 <= x4 && y2 <= y3 && y2 >= y4 = True -- ponto superior direito
-                                                  | x1 >= x3 && x1 <= x4 && y2 <= y3 && y2 >= y4 = True -- ponto superior esquerdo
-                                                  | x2 >= x3 && x2 <= x4 && y1 <= y3 && y1 >= y4 = True -- ponto inferior direito
+colisaoHitbox ((x1,y1),(x2,y2)) ((x3,y3),(x4,y4)) | x1 >= x3 && x1 <= x4 && y2 >= y3 && y2 <= y4 = True -- ponto inferior esquerdo
+                                                  | x2 >= x3 && x2 <= x4 && y1 >= y3 && y1 <= y4 = True -- ponto superior direito
+                                                  | x1 >= x3 && x1 <= x4 && y1 >= y3 && y1 <= y4 = True -- ponto superior esquerdo
+                                                  | x2 >= x3 && x2 <= x4 && y2 >= y3 && y2 <= y4 = True -- ponto inferior direito
                                                   | otherwise = False
 
 
