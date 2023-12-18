2a48eb8bd9ac4c26b769f47725ebbf5d37acaa4d
a107382 <a107382@alunos.uminho.pt>
2023-12-18 12:30:10 +0000

    início da definição de funções da Tarefa 4

--- lib/Tarefa4.hs
+++ lib/Tarefa4.hs
@@ -9,8 +9,40 @@ Módulo para a realização da Tarefa 4 de LI1 em 2023/24.
 module Tarefa4 where
 
 import Data.Maybe
-
+import Tarefa1
+import Tarefa2
+import Tarefa3
 import LI12324
 
 atualiza :: [Maybe Acao] -> Maybe Acao -> Jogo -> Jogo
 atualiza = undefined
+
+atualizaInimigos :: Mapa -> Personagem -> Personagem
+atualizaInimigos m p = undefined
+
+atualizaJogador :: Mapa -> Personagem -> Personagem
+atualizaJogador m p = undefined
+
+movePersonagem :: Personagem -> Acao -> Personagem
+movePersonagem p a = case a of
+                        Subir -> usaEscada p a
+                        Descer -> usaEscada p a
+                        AndarDireita -> moveDireita p a
+                        AndarEsquerda -> moveEsquerda p a
+                        Saltar -> salta p a
+                        Parar -> para p a
+
+usaEscada :: Personagem -> Acao -> Personagem
+usaEscada p a = undefined
+
+moveDireita :: Personagem -> Acao -> Personagem
+moveDireita p a = undefined
+
+moveEsquerda :: Personagem -> Acao -> Personagem
+moveEsquerda p a = undefined
+
+salta :: Personagem -> Acao -> Personagem
+salta p a = undefined
+
+para :: Personagem -> Acao -> Personagem
+para p a = undefined
