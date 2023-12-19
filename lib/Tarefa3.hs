53005c7ef9a6db6eabb006bab108e8d320129737
andreia <a106915@alunos.uminho.pt>
2023-12-19 09:31:00 +0000

    melhorias na documentação existente

--- lib/Tarefa3.hs
+++ lib/Tarefa3.hs
@@ -31,11 +31,25 @@ hitboxDano (Personagem {posicao = (x,y), tamanho = (l,a), direcao = dir}) | dir 
                                                                           | dir == Este = ((x + (l/2),y-(a/2)),(x + (3*l/2),y + (a/2)))
 {-|
 
-(Jogo {mapa = Mapa ((2,0),Norte) (1,2) [[Escada,Alcapao,Vazio],[Escada,Vazio,Plataforma],[Plataforma,Plataforma,Plataforma]],inimigos = [Personagem {velocidade = (1,2),tipo = Fantasma,posicao = (1,1),direcao = Oeste, tamanho = (2,2),ressalta = True ,vida = 1,emEscada = False, pontos = 0,aplicaDano = (False,0)},Personagem {tipo = Fantasma,posicao = (0,0),ressalta = True,vida = 1, velocidade = (1,2), direcao = Oeste, tamanho = (2,2), emEscada = False, pontos = 0, aplicaDano = (False,0)}],colecionaveis = [(Martelo, (1,1)),(Moeda, (2,0))],jogador = Personagem {velocidade = (1,2),posicao = (2,0),tamanho = (1,2),ressalta = False, tipo = Jogador, emEscada = False, direcao = Oeste, vida = 3, pontos = 0, aplicaDano = (True,5)}})
+Se o jogador tiver a componente aplicaDano activa e com tempo restante e a 
+hitbox de dano do jogador colidir com um inimigo retira uma vida ao inimigo
 
+=Exemplos
 
-
+>>>movimenta1 (Jogo {mapa = Mapa ((2.5,1),Oeste) (2.5,1) [[Escada,Alcapao,Vazio],[Escada,Vazio,Plataforma],[Plataforma,Plataforma,Plataforma]]
+                    ,inimigos = [Personagem {velocidade = (1,2),tipo = Fantasma,posicao = (1.5,1.5),direcao = Oeste, tamanho = (1,1),emEscada = False,ressalta = True,vida = 1, pontos = 0, aplicaDano = (False,0)}
+                                ,Personagem {velocidade = (1,2),tipo = Fantasma,posicao = (4,1),direcao = Oeste, tamanho = (1,1),emEscada = False,ressalta = True,vida = 1, pontos = 0, aplicaDano = (False,0)}]
+                    ,colecionaveis = [(Martelo, (1,1)),(Moeda, (2,0))]
+                    ,jogador = Personagem {velocidade = (1,2),tipo = Jogador,posicao = (2.5,1),direcao = Oeste, tamanho = (1,2),emEscada = False,ressalta = False, vida = 3, pontos = 0, aplicaDano = (True,5)}})
+=Jogo {mapa = Mapa ((2.5,1.0),Norte) (2.5,1.0) [[Escada,Alcapao,Vazio],[Escada,Vazio,Plataforma],[Plataforma,Plataforma,Plataforma]]
+      ,inimigos = [Personagem {velocidade = (1.0,2.0), tipo = Fantasma, posicao = (1.5,1.5), direcao = Oeste, tamanho = (1.0,1.0), emEscada = False, ressalta = True, vida = 0, pontos = 0, aplicaDano = (False,0.0)}
+                  ,Personagem {velocidade = (1.0,2.0), tipo = Fantasma, posicao = (4.0,1.0), direcao = Oeste, tamanho = (1.0,1.0), emEscada = False, ressalta = True, vida = 1, pontos = 0, aplicaDano = (False,0.0)}]
+      ,colecionaveis = [(Martelo,(1.0,1.0)),(Moeda,(2.0,0.0))]
+      ,jogador = Personagem {velocidade = (1.0,2.0), tipo = Jogador, posicao = (2.5,1.0), direcao = Oeste, tamanho = (1.0,2.0), emEscada = False, ressalta = False, vida = 3, pontos = 0, aplicaDano = (True,5.0)}}
+    
 -}
+
+
 movimenta1 :: Jogo -> Jogo
 movimenta1 (Jogo {mapa = m ,inimigos = listaInimigos,colecionaveis = listaColecionaveis,jogador = jog}) 
         | fst (aplicaDano jog) == True && snd (aplicaDano jog) > 0 = Jogo {mapa = m 
@@ -58,52 +72,12 @@ movimenta1 (Jogo {mapa = m ,inimigos = listaInimigos,colecionaveis = listaColeci
 Se a hitbox de dano do jogador colidir com um inimigo retira uma vida ao inimigo
 
 =Exemplos
->>>ataqueJogador [Personagem {velocidade = (1,2)
-                             ,tipo = Fantasma
-                             ,posicao = (7,7)
-                             ,direcao = Este
-                             ,tamanho = (2,2)
-                             ,emEscada = False
-                             ,ressalta = True
-                             ,vida = 1
-                             ,pontos = 0
-                             ,aplicaDano = (False,0)}
-                 ,Personagem {velocidade = (1,2)
-                             ,tipo = Fantasma
-                             ,posicao = (1,1)
-                             ,direcao = Este
-                             ,tamanho = (2,2)
-                             ,emEscada = False
-                             ,ressalta = True
-                             ,vida = 1,pontos = 0
-                             ,aplicaDano = (False,0)
-                             }
-                 ] 
-                 (Personagem {posicao = (0.5,1)
-                             ,direcao = Este
-                             ,tamanho = (1,2)
-                             ,aplicaDano = (True,5)})
+>>>ataqueJogador [Personagem {velocidade = (1,2),tipo = Fantasma,posicao = (7,7),direcao = Este,tamanho = (2,2),emEscada = False,ressalta = True,vida = 1,pontos = 0,aplicaDano = (False,0)}
+                 ,Personagem {velocidade = (1,2),tipo = Fantasma,posicao = (1,1),direcao = Este,tamanho = (2,2),emEscada = False,ressalta = True,vida = 1,pontos = 0,aplicaDano = (False,0)}] 
+                 (Personagem {posicao = (0.5,1),direcao = Este,tamanho = (1,2),aplicaDano = (True,5)})
                  =
-                [Personagem {velocidade = (1.0,2.0)
-                            ,tipo = Fantasma
-                            ,posicao = (1.0,1.0)
-                            ,direcao = Este
-                            ,tamanho = (2.0,2.0)
-                            ,emEscada = False
-                            ,ressalta = True
-                            ,vida = 0
-                            ,pontos = 0
-                            ,aplicaDano = (False,0.0)}
-                ,Personagem {velocidade = (1.0,2.0)
-                            ,tipo = Fantasma
-                            ,posicao = (1.0,1.0)
-                            ,direcao = Este
-                            ,tamanho = (2.0,2.0)
-                            ,emEscada = False
-                            ,ressalta = True
-                            ,vida = 0
-                            ,pontos = 0
-                            ,aplicaDano = (False,0.0)}]
+                [Personagem {velocidade = (1.0,2.0),tipo = Fantasma,posicao = (1.0,1.0),direcao = Este,tamanho = (2.0,2.0),emEscada = False,ressalta = True,vida = 0,pontos = 0,aplicaDano = (False,0.0)}
+                ,Personagem {velocidade = (1.0,2.0),tipo = Fantasma,posicao = (1.0,1.0),direcao = Este,tamanho = (2.0,2.0),emEscada = False,ressalta = True,vida = 0,pontos = 0,aplicaDano = (False,0.0)}]
 -}
 
 ataqueJogador :: [Personagem] -> Personagem -> [Personagem] -- terá de ser usada numa futura função que recebe um jogo e verifica se a aplicaDano do Jogador tá ativa
@@ -134,49 +108,16 @@ colisaoHitbox ((x1,y1),(x2,y2)) ((x3,y3),(x4,y4)) | x1 >= x3 && x1 <= x4 && y2 >
 Se o jogador colidir com um inimigo retira uma vida ao jogador
 
 =Exemplos
->>> ataqueInimigo [Personagem {posicao = (1,1)
-                              ,tamanho = (2,2)
-                              }
-                  ,Personagem {posicao = (4,5)
-                              ,tamanho = (2,2)
-                              }
-                   ] 
-                   (Personagem {velocidade = (1,2)
-                               ,tipo = Jogador
-                               ,posicao = (1,1)
-                               ,direcao = Este
-                               ,tamanho = (2,2)
-                               ,emEscada = False
-                               ,ressalta = True
-                               ,vida = 1
-                               ,pontos= 0
-                               ,aplicaDano = (False,0)
-                               }
-                    )
-                    = 
-                    Personagem {velocidade = (1.0,2.0)
-                               ,tipo = Jogador
-                               ,posicao = (1.0,1.0)
-                               ,direcao = Este
-                               ,tamanho = (2.0,2.0)
-                               ,emEscada = False
-                               ,ressalta = True
-                               ,vida = 0
-                               ,pontos = 0
-                               ,aplicaDano = (False,0.0)
-                               }
+>>> ataqueInimigo [Personagem {posicao = (1,1),tamanho = (2,2)}
+                  ,Personagem {posicao = (4,5),tamanho = (2,2)}] 
+                  (Personagem {velocidade = (1,2),tipo = Jogador,posicao = (1,1),direcao = Este,tamanho = (2,2),emEscada = False,ressalta = True,vida = 1,pontos= 0,aplicaDano = (False,0)})
+                   = 
+                   Personagem {velocidade = (1.0,2.0),tipo = Jogador,posicao = (1.0,1.0),direcao = Este,tamanho = (2.0,2.0),emEscada = False,ressalta = True,vida = 0,pontos = 0,aplicaDano = (False,0.0)}
 -}
 ataqueInimigo :: [Personagem] -> Personagem -> Personagem 
 ataqueInimigo [] p = p
 ataqueInimigo (inim : t) jog | colisoesPersonagens inim jog = (jog {vida = vida jog -1})
                              | otherwise = ataqueInimigo t jog
-
-
-
-
-
-
-
 
 
 
