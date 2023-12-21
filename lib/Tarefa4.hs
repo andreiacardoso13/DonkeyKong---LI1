c7a8b50acb460e4aca904d362f82c42aed2e5ce3
a107382 <a107382@alunos.uminho.pt>
2023-12-21 13:03:59 +0000

    finalização da documentação das funções da Tarefa 4

--- lib/Tarefa4.hs
+++ lib/Tarefa4.hs
@@ -18,8 +18,16 @@ import LI12324
 
 = Exemplos
 
->>>
->>>
+>>> atualiza [Just Parar, Nothing] (Just AndarDireita) (Jogo {mapa = Mapa ((2.5,1),Oeste) (2.5,1) [[Vazio,Plataforma,Vazio],[Vazio,Escada,Vazio],[Plataforma,Plataforma,Plataforma]],
+                                                         inimigos = [Personagem {velocidade = (0,10),tipo = Fantasma,posicao = (1.5,1.5),direcao = Norte, tamanho = (1,1),emEscada = True,ressalta = True,vida = 1, pontos = 0, aplicaDano = (False,0)},
+                                                                     Personagem {velocidade = (10,0),tipo = Fantasma,posicao = (0.5,1.5),direcao = Este, tamanho = (1,1),emEscada = False,ressalta = True,vida = 1, pontos = 0, aplicaDano = (False,0)}],
+                                                         colecionaveis = [(Martelo, (1,1)),(Moeda, (2,0))],
+                                                         jogador = Personagem {velocidade = (-10,0),tipo = Jogador,posicao = (2.5,1.5),direcao = Oeste, tamanho = (1,1),emEscada = False,ressalta = False, vida = 3, pontos = 0, aplicaDano = (True,5)}})
+             = Jogo {mapa = Mapa ((2.5,1.0),Oeste) (2.5,1.0) [[Vazio,Plataforma,Vazio],[Vazio,Escada,Vazio],[Plataforma,Plataforma,Plataforma]],
+                     inimigos = [Personagem {velocidade = (0.0,0.0), tipo = Fantasma, posicao = (1.5,1.5), direcao = Norte, tamanho = (1.0,1.0), emEscada = True, ressalta = True, vida = 1, pontos = 0, aplicaDano = (False,0.0)},
+                                 Personagem {velocidade = (10.0,0.0), tipo = Fantasma, posicao = (0.5,1.5), direcao = Este, tamanho = (1.0,1.0), emEscada = False, ressalta = True, vida = 1, pontos = 0, aplicaDano = (False,0.0)}],
+                     colecionaveis = [(Martelo,(1.0,1.0)),(Moeda,(2.0,0.0))],
+                     jogador = Personagem {velocidade = (10.0,0.0), tipo = Jogador, posicao = (2.5,1.5), direcao = Este, tamanho = (1.0,1.0), emEscada = False, ressalta = False, vida = 3, pontos = 0, aplicaDano = (True,5.0)}}
 -}
 
 atualiza :: [Maybe Acao] -> Maybe Acao -> Jogo -> Jogo
@@ -43,8 +51,11 @@ atualizaInimigos (i:is) (ai:ais) =  movePersonagem i ai : atualizaInimigos is ai
 
 = Exemplos
 
->>>
->>>
+>>> movePersonagem (Personagem {velocidade = (10,0), tipo = Fantasma, posicao = (3,2), direcao = Este, tamanho = (1,1), emEscada = True, ressalta = True, vida = 1, pontos = 0, aplicaDano = (False,0)}) (Just Parar)
+Personagem {velocidade = (0.0,0.0), tipo = Fantasma, posicao = (3.0,2.0), direcao = Este, tamanho = (1.0,1.0), emEscada = True, ressalta = True, vida = 1, pontos = 0, aplicaDano = (False,0.0)}
+
+>>> movePersonagem (Personagem {velocidade = (10,0), tipo = Jogador, posicao = (3,2), direcao = Este, tamanho = (1,1), emEscada = False, ressalta = False, vida = 2, pontos = 0, aplicaDano = (False,0)}) (Nothing)
+Personagem {velocidade = (10.0,0.0), tipo = Fantasma, posicao = (3.0,2.0), direcao = Este, tamanho = (1.0,1.0), emEscada = False, ressalta = False, vida = 2, pontos = 0, aplicaDano = (False,0.0)}
 -}
 
 movePersonagem :: Personagem -> Maybe Acao -> Personagem
@@ -57,28 +68,28 @@ movePersonagem p a = case a of
                         Just Parar -> para p Parar
                         Nothing -> p
 
--- | Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'Subir' ou 'Descer'. 
+{-| Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'Subir' ou 'Descer'. -}
 
 usaEscada :: Personagem -> Acao -> Personagem
 usaEscada p a | a == Subir =  (p {velocidade = (0,-10), direcao = Norte})
               | a == Descer = (p {velocidade = (0, 10), direcao = Norte})
 
--- | Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'AndarDireita'. 
+{-| Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'AndarDireita'. -}
 
 moveDireita :: Personagem -> Acao -> Personagem
 moveDireita p AndarDireita = (p {velocidade = (10,0), direcao = Este})
 
--- | Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'AndarEsquerda'. 
+{-| Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'AndarEsquerda'. -}
 
 moveEsquerda :: Personagem -> Acao -> Personagem
 moveEsquerda p AndarEsquerda = (p {velocidade = (-10,0), direcao = Oeste})
 
--- | Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'Saltar'. 
+{-| Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'Saltar'. -}
 
 salta :: Personagem -> Acao -> Personagem
 salta p a = undefined
 
--- | Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'Parar'. 
+{-| Atualiza a velocidade e direção de uma personagem ao aplicar a ação 'Parar'. -}
 
 para :: Personagem -> Acao -> Personagem
 para p Parar = (p {velocidade = (0,0)})
