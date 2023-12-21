a75d417de7611c3ee3242e154b28ae449242010a
a107382 <a107382@alunos.uminho.pt>
2023-12-21 15:15:40 +0000

    criação do ficheiro para o módulo da Tarefa 5

---
+++ lib/Tarefa5.hs
@@ -0,0 +1,9 @@
+{-|
+Module      : Tarefa5
+Description : 
+Copyright   : Andreia Alves Cardoso <a106915@alunos.uminho.pt>
+              Cátia Alexandra Ribeiro da Eira <a107382@alunos.uminho.pt>
+
+Módulo para a realização da Tarefa 5 de LI1 em 2023/24.
+-}
+module Tarefa5 where
