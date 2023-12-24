b86c60c9c97589dee7eb1c8562957bc0da61154c
andreia <a106915@alunos.uminho.pt>
2023-12-24 14:05:49 +0000

    correção de erros causados pela alteração do Estado

--- lib/Main.hs
+++ lib/Main.hs
@@ -4,6 +4,7 @@ import Graphics.Gloss.Interface.Pure.Game
 import LI12324
 import Imagens
 import Mapa
+import Data.Maybe (fromMaybe)
 import Graphics.Gloss.Juicy -- precisa apenas de ficar no Imagens ACHO
 
 
@@ -14,7 +15,7 @@ main = do
 
 data Estado = Estado {jogo :: Jogo}
 
-type EstadoGloss = (Estado,Picture)
+type EstadoGloss = (Estado,[(Bloco, (Maybe Picture,(Float,Float)))])
 
 
 
@@ -24,14 +25,15 @@ main = do
         maybeEscada <- loadJuicyPNG "imagens/ladder.png"
         case maybeEscada of 
           Just escada -> play
-              janela                          -- janela onde irá decorrer o jogo
-              bg                              -- cor do fundo da janela
-              fr                              -- frame rate
-              (estadoGlossInicial maybeEscada)                 -- define estado inicial do jogo
-              desenhaEstado                   -- desenha o estado do jogo
-              reageEvento                     -- reage a um evento
-              reageTempo                      -- reage ao passar do tempo
+              janela                                          -- janela onde irá decorrer o jogo
+              bg                                       -- cor do fundo da janela
+              fr                                          -- frame rate
+              (estadoGlossInicial [(Escada, (maybeEscada,(0,0)))])                 -- define estado inicial do jogo
+              desenhaEstado                                                   --desenha o estado do jogo
+              reageEvento                                           -- reage a um evento
+              reageTempo                                      -- reage ao passar do tempo
           Nothing -> putStrLn "Erro ao carregar imagem"
+
 
 janela :: Display
 janela = InWindow
@@ -48,13 +50,19 @@ fr = 20
 estadoInicial :: Estado
 estadoInicial = Estado {jogo = j1}
 
-estadoGlossInicial :: Maybe Picture -> EstadoGloss
-estadoGlossInicial maybeImagem = case maybeImagem of
-  Just imagem -> (estadoInicial,imagem)
-  Nothing -> error "Erro ao carregar imagem"
+estadoGlossInicial :: [(Bloco, (Maybe Picture, (Float, Float)))] -> EstadoGloss
+estadoGlossInicial textures = (estadoInicial, textures' textures)
+  where
+    textures' :: [(Bloco, (Maybe Picture, (Float, Float)))] -> [(Bloco, (Maybe Picture, (Float, Float)))]
+    textures' [] = []
+    textures' ((bloco, (maybePicture, (a, b))) : t) = (bloco, (maybePicture, (realToFrac a, realToFrac b))) : textures' t
 
 desenhaEstado :: EstadoGloss -> Picture
-desenhaEstado (jog,z) = z
+desenhaEstado (_, [(_, (aMaybe, (_, _)))]) =
+  case aMaybe of
+    Just a -> a
+    Nothing -> rectangleSolid 50 50
+
 
 reageEvento :: Event -> EstadoGloss -> EstadoGloss
 reageEvento _ s = s
