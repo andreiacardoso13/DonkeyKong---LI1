7e8540519374834f47349d038b089b8c910be954
andreia <a106915@alunos.uminho.pt>
2023-12-29 13:35:31 +0000

    função desenhaJogador

--- lib/Main.hs
+++ lib/Main.hs
@@ -45,26 +45,33 @@ estadoInicial :: Imagens -> Estado
 estadoInicial images = Estado {jogo = j1, imagens = images}
 
 desenhaEstado :: Estado -> Picture
-desenhaEstado s = Pictures(desenhaMapa1 (-715.5,450.5) s)
+desenhaEstado s = Pictures((desenhaMapa1 (-715.5,450.5) s)++desenhaJogador s)
 
 desenhaMapa1 :: (Float,Float) -> Estado -> [Picture]
 desenhaMapa1 _ (Estado {jogo = (Jogo {mapa = Mapa a b []})}) = []
 desenhaMapa1 (x,y) (Estado {jogo = (Jogo {mapa = Mapa a b (h:t)}), imagens = imgs}) = (desenhaLinhas1 (x,y) imgs h) ++ (desenhaMapa1 (x,y-53) (Estado {jogo = (Jogo {mapa = Mapa a b t}), imagens = imgs}))
 
-desenhaLinhas1 :: (Float,Float) -> Imagens -> [Bloco] -> [Picture] -- Posicao é o tuplo para o Translate (-715.5,-450.5)
+desenhaLinhas1 :: (Float,Float) -> Imagens -> [Bloco] -> [Picture] 
 desenhaLinhas1 _ _ [] = []
 desenhaLinhas1 (x,y) imgs (h : t) | h == Escada = (Translate x y (getImagem Ladder imgs)) : desenhaLinhas1 (x+53,y) imgs t 
                                   | h == Plataforma = (Translate x y(getImagem Platform imgs)): desenhaLinhas1 (x+53,y) imgs t 
                                   | h == Alcapao = (Translate x y(getImagem Trapdoor imgs)): desenhaLinhas1 (x+53,y) imgs t 
                                   | h == Vazio = desenhaLinhas1 (x+53,y) imgs t 
 
+desenhaJogador :: Estado -> [Picture]
+--desenhaJogador (Estado {jogo = (Jogo {jogador = jog}), imagens = img})
+desenhaJogador est | (direcao (jogador (jogo (est)))) == Norte = [getImagem MarioWalkingRight1 (imagens est)]
+                   | otherwise = [getImagem Estrela (imagens est)]
 
+
+
+{-
 converteCoordenadas :: Posicao -> Posicao 
 converteCoordenadas (x,y) = (x - 742, y - 477)
 
 converteCoordenadasMapa :: Posicao -> Posicao 
 converteCoordenadasMapa (x,y) = (x - 715.5, y - 450.5)
-
+-}
 
 reageEvento :: Event -> Estado -> Estado
 reageEvento _ s = s
