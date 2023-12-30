b264bf76fb45e0e3b041189f26eefb258846bf6c
a107382 <a107382@alunos.uminho.pt>
2023-12-30 18:15:21 +0000

    edição da função keys e adição de alguns casos de paragem

--- lib/Tarefa5.hs
+++ lib/Tarefa5.hs
@@ -23,25 +23,21 @@ data Estado = Estado {jogo :: Jogo, imagens :: Imagens, tempo :: Float} -- o tem
 keys :: Event -> Estado -> Estado
 keys (EventKey (SpecialKey KeyRight) Down _ _) e@(Estado {jogo = j@(Jogo {mapa = m@(Mapa _ _ blocos), 
                                                                           jogador = jgd@(Personagem {posicao = pos@(x,y), 
-                                                                                                     emEscada = esc})})}) = e {jogo = j {jogador = jgd {posicao = newpos, 
-                                                                                                                                                        emEscada = procuraBloco blocos newpos == Escada}}}
-                                                                                                                            where newpos = (min (x+1) 27.5, y)
+                                                                                                     emEscada = esc})})}) | not esc = e {jogo = j {jogador = movePersonagem (jgd {posicao = (min (x+1) 27.5, y)}) (Just AndarDireita)}}
+                                                                                                                          | otherwise = e
 
-keys (EventKey (SpecialKey KeyLeft)  Down _ _) e@(Estado {jogo = j@(Jogo {mapa = m@(Mapa _ _ blocos), 
-                                                                          jogador = jgd@(Personagem {posicao = pos@(x,y), 
-                                                                                                     emEscada = esc})})}) = e {jogo = j {jogador = jgd {posicao = newpos, 
-                                                                                                                                                        emEscada = procuraBloco blocos newpos == Escada}}}
-                                                                                                                            where newpos = (max (x-1) 0.5, y)
+keys (EventKey (SpecialKey KeyLeft) Down _ _) e@(Estado {jogo = j@(Jogo {mapa = m@(Mapa _ _ blocos), 
+                                                                         jogador = jgd@(Personagem {posicao = pos@(x,y), 
+                                                                                                    emEscada = esc})})}) | not esc = e {jogo = j {jogador = movePersonagem (jgd {posicao = (max (x-1) 0.5, y)}) (Just AndarEsquerda)}}
+                                                                                                                         | otherwise = e
 
 keys (EventKey (SpecialKey KeyUp)    Down _ _) e@(Estado {jogo = j@(Jogo {mapa = m@(Mapa _ _ blocos), 
                                                                           jogador = jgd@(Personagem {posicao = pos@(x,y), 
-                                                                                                     emEscada = esc})})}) | esc && procuraBlocoSup blocos pos == Plataforma = e {jogo = j {jogador = jgd {posicao = newpos', 
-                                                                                                                                                                                                          emEscada = procuraBloco blocos newpos' == Escada}}}
-                                                                                                                          | esc                                             = e {jogo = j {jogador = jgd {posicao = newpos,
-                                                                                                                                                                                                          emEscada = procuraBloco blocos newpos == Escada}}}
+                                                                                                     emEscada = esc})})}) | esc && procuraBlocoSup blocos pos == Plataforma = e {jogo = j {jogador = jgd {posicao = (x, max (y-2) 0.5), 
+                                                                                                                                                                                                          emEscada = esc}}}
+                                                                                                                          | esc                                             = e {jogo = j {jogador = jgd {posicao = (x, max (y-1) 0.5),
+                                                                                                                                                                                                          emEscada = esc}}}
                                                                                                                           | otherwise = e
-                                                                                                                                        where newpos  = (x, max (y-1) 0.5)
-                                                                                                                                              newpos' = (x, max (y-2) 0.5)
 
 keys (EventKey (SpecialKey KeyDown)  Down _ _) e@(Estado {jogo = j@(Jogo {mapa = m@(Mapa _ _ blocos), 
                                                                           jogador = jgd@(Personagem {posicao = pos@(x,y), 
@@ -51,6 +47,16 @@ keys (EventKey (SpecialKey KeyDown)  Down _ _) e@(Estado {jogo = j@(Jogo {mapa =
                                                                                                                                                                               where newpos = (x, min (y+1) 16.5)
 
 keys (EventKey (SpecialKey KeySpace) Down _ _) e@(Estado {jogo = j@(Jogo {mapa = m, jogador = jgd@(Personagem {posicao = pos@(x,y)})})}) = e {jogo = j {jogador = jgd {posicao = (x, y+2)}}}
+
+keys (EventKey (SpecialKey KeyRight) Up _ _) e@(Estado {jogo = j@(Jogo {mapa = m@(Mapa _ _ blocos), 
+                                                                          jogador = jgd@(Personagem {posicao = pos@(x,y), 
+                                                                                                     emEscada = esc})})}) = e {jogo = j {jogador = movePersonagem jgd (Just Parar)}}
+
+keys (EventKey (SpecialKey KeyLeft) Up _ _) e@(Estado {jogo = j@(Jogo {mapa = m@(Mapa _ _ blocos), 
+                                                                          jogador = jgd@(Personagem {posicao = pos@(x,y), 
+                                                                                                     emEscada = esc})})}) = e {jogo = j {jogador = movePersonagem jgd (Just Parar)}}
+
+
 keys _ e = e
 
 -- EventKey Key KeyState Modifiers (Float, Float)
