7b8c53c2bd21336e26d0b8f847124f57b8c57a67
andreia <a106915@alunos.uminho.pt>
2023-12-30 01:57:27 +0000

    Atualização das posições dos colecionáveis no jogo de exemplo

--- lib/Mapa.hs
+++ lib/Mapa.hs
@@ -89,7 +89,7 @@ j1 = Jogo {mapa = mapaInicial
                                   ,aplicaDano=(False,0)
                                    }
                       ]
-          ,colecionaveis = [(Moeda,(2.5,0.5)),(Martelo, (3.5,4.5))]
+          ,colecionaveis = [(Moeda,(17.5,7.5)),(Martelo, (8.5,10.5))]
           ,jogador = Personagem {velocidade=(1,0)
                                 ,tipo=Jogador
                                 ,posicao=(0.5,16.5)
