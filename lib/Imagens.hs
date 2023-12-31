f99fca93bf4134735c85b094b65b7f493e4c3de6
a107382 <a107382@alunos.uminho.pt>
2023-12-31 10:48:20 +0000

    adição de imagens em falta (Mário derrotado, fastasmas derrotados, e macaco em pé)

--- lib/Imagens.hs
+++ lib/Imagens.hs
@@ -2,7 +2,7 @@ module Imagens where
 import Graphics.Gloss
 import Graphics.Gloss.Juicy
 
-data Imagem = Ladder | Trapdoor | Platform | Estrela | Coin | Hammer | GhostLeft1 | GhostLeft2 | GhostRight1 | GhostRight2 | MarioClimbing1 | MarioClimbing2 | MarioHammerLeft1 | MarioHammerLeft2 | MarioHammerLeft3 | MarioHammerLeft4 | MarioHammerRight1 | MarioHammerRight2 | MarioHammerRight3 | MarioHammerRight4 | MarioJumpingLeft1 | MarioJumpingRight1 | MarioStandingBack | MarioStandingLeft | MarioStandingRight | MarioWalkingLeft1 | MarioWalkingRight1 | MonkeyDefeated | MonkeyFalling | VidaCheia | VidaVazia | ZeroVidas | UmaVida | DuasVidas | TresVidas | Num0 | Num1 | Num2 | Num3 | Num4 | Num5 | Num6 | Num7 | Num8 | Num9 | Score | Bonus deriving (Eq)
+data Imagem = Ladder | Trapdoor | Platform | Estrela | Coin | Hammer | GhostLeft1 | GhostLeft2 | GhostRight1 | GhostRight2 | MarioClimbing1 | MarioClimbing2 | MarioHammerLeft1 | MarioHammerLeft2 | MarioHammerLeft3 | MarioHammerLeft4 | MarioHammerRight1 | MarioHammerRight2 | MarioHammerRight3 | MarioHammerRight4 | MarioJumpingLeft1 | MarioJumpingRight1 | MarioStandingBack | MarioStandingLeft | MarioStandingRight | MarioWalkingLeft1 | MarioWalkingRight1 | MonkeyDefeated | MonkeyFalling | MonkeyStanding | VidaCheia | VidaVazia | ZeroVidas | UmaVida | DuasVidas | TresVidas | Num0 | Num1 | Num2 | Num3 | Num4 | Num5 | Num6 | Num7 | Num8 | Num9 | Score | Bonus | MarioDefeated1 | MarioDefeated2 | MarioDefeated3 | MarioDefeated4 | MarioDefeatedFinal | GhostDefeated1 | GhostDefeated2 | GhostDefeated3 | GhostDefeated4 deriving (Eq)
 
 type Imagens = [(Imagem, Picture)]
 
@@ -38,6 +38,7 @@ getImages = do
                Just marioWalkingRight1  <- loadJuicyPNG "imagens/marioWalkingRight1.png"
                Just monkeyDefeated      <- loadJuicyPNG "imagens/monkeyDefeated.png"
                Just monkeyFalling       <- loadJuicyPNG "imagens/monkeyFalling.png"
+               Just monkeyStanding      <- loadJuicyPNG "imagens/monkeyStanding.png"
                Just vidaCheia           <- loadJuicyPNG "imagens/vidacheia.png"
                Just vidaVazia           <- loadJuicyPNG "imagens/vidavazia.png"
                Just zeroVidas           <- loadJuicyPNG "imagens/vida0.png"
@@ -56,6 +57,15 @@ getImages = do
                Just num9                <- loadJuicyPNG "imagens/num9.png"
                Just score               <- loadJuicyPNG "imagens/score.png"
                Just bonus               <- loadJuicyPNG "imagens/bonus.png"
+               Just marioDefeated1      <- loadJuicyPNG "imagens/marioDefeated1.png" 
+               Just marioDefeated2      <- loadJuicyPNG "imagens/marioDefeated2.png"
+               Just marioDefeated3      <- loadJuicyPNG "imagens/marioDefeated3.png"
+               Just marioDefeated4      <- loadJuicyPNG "imagens/marioDefeated4.png"
+               Just marioDefeatedFinal  <- loadJuicyPNG "imagens/marioDefeatedFinal.png"
+               Just ghostDefeated1      <- loadJuicyPNG "imagens/ghostDefeated1.png"
+               Just ghostDefeated2      <- loadJuicyPNG "imagens/ghostDefeated2.png"
+               Just ghostDefeated3      <- loadJuicyPNG "imagens/ghostDefeated3.png"
+               Just ghostDefeated4      <- loadJuicyPNG "imagens/ghostDefeated4.png"
 
                let images = [(Ladder,escada), (Trapdoor,alcapao), (Platform,plataforma), (Estrela,estrela),
                              (Coin,moeda), (Hammer,martelo), (GhostLeft1,ghostLeft1), (GhostLeft2,ghostLeft2),
@@ -68,9 +78,12 @@ getImages = do
                              (MarioJumpingRight1,marioJumpingRight1), (MarioStandingBack,marioStandingBack),
                              (MarioStandingLeft,marioStandingLeft), (MarioStandingRight,marioStandingRight),
                              (MarioWalkingLeft1,marioWalkingLeft1), (MarioWalkingRight1,marioWalkingRight1),
-                             (MonkeyDefeated,monkeyDefeated), (MonkeyFalling,monkeyFalling), (VidaCheia,vidaCheia),
-                             (VidaVazia,vidaVazia), (ZeroVidas,zeroVidas), (UmaVida,umaVida), (DuasVidas,duasVidas),
-                             (TresVidas,tresVidas), (Num0,num0), (Num1,num1), (Num2,num2), (Num3,num3), (Num4,num4),
-                             (Num5,num5), (Num6,num6), (Num7,num7), (Num8,num8), (Num9,num9), (Score,score),
-                             (Bonus,bonus)]
+                             (MonkeyDefeated,monkeyDefeated), (MonkeyFalling,monkeyFalling), (MonkeyStanding,monkeyStanding),
+                             (VidaCheia,vidaCheia), (VidaVazia,vidaVazia), (ZeroVidas,zeroVidas), (UmaVida,umaVida),
+                             (DuasVidas,duasVidas), (TresVidas,tresVidas), (Num0,num0), (Num1,num1), (Num2,num2),
+                             (Num3,num3), (Num4,num4), (Num5,num5), (Num6,num6), (Num7,num7), (Num8,num8), (Num9,num9),
+                             (Score,score), (Bonus,bonus), (MarioDefeated1,marioDefeated1), (MarioDefeated2,marioDefeated2),
+                             (MarioDefeated3,marioDefeated3), (MarioDefeated4,marioDefeated4), (MarioDefeatedFinal,marioDefeatedFinal),
+                             (GhostDefeated1,ghostDefeated1), (GhostDefeated2,ghostDefeated2), (GhostDefeated3,ghostDefeated3),
+                             (GhostDefeated4,ghostDefeated4)]
                return images
