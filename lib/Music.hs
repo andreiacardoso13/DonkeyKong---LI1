806afb5660eb1f36df08aef4f6c25b1e3c8c05b0
a107382 <a107382@alunos.uminho.pt>
2024-01-11 16:55:36 +0000

    remoção de ficheiro mp3 não utilizado

--- lib/Music.hs
+++ lib/Music.hs
@@ -5,8 +5,5 @@ import System.Process
 musicaMenu :: IO ProcessHandle
 musicaMenu = spawnCommand "mpv --no-video --loop music/Title-BGM.mp3"
 
-musicaJogo :: IO ProcessHandle
-musicaJogo = spawnCommand "mpv --no-video --loop music/Stage-1-BGM.mp3"
-
 musicaParar :: IO ProcessHandle
 musicaParar = spawnCommand "killall mpv"
