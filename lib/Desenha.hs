e9573e3b8378f8d258df576856f9577775e74a59
andreia <a106915@alunos.uminho.pt>
2024-01-14 21:41:34 +0000

    Correção de erro nos imports

--- lib/Desenha.hs
+++ lib/Desenha.hs
@@ -1,16 +1,10 @@
 module Desenha where 
 
---import Graphics.Gloss
---import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Interface.IO.Game
 import LI12324
---import Tarefa1
---import Tarefa2
---import Tarefa3
 import Tarefa5
 import Imagens
 import Mapa
---import Music
 import Data.Fixed
 import Data.List
 
